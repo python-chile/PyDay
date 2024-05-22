ba12bcda690fd0c7e3e71267f86e37f710982d31
Aldo Caneo <aldo.caneo@pythonchile.cl>
2024-05-22 10:01:48 -0400

    add version to copiapo schedule

--- assets/js/copiapo.js
+++ assets/js/copiapo.js
@@ -9,5 +9,5 @@ const FILES =[
 for(let i=0; i<FILES.length;i++){
     await fetch('assets/sections/copiapo/' + FILES[i] + '?v=1.0').then(r => r.text()).then(r => document.body.innerHTML+=r)
 }
-import schedule from '/assets/js/copiapo/schedule.js';
+import schedule from '/assets/js/copiapo/schedule.js?v=1.0';
 schedule()
